3a96b3bec277342fb17e377e9cb14d6c9e505a07
Keshav Mohta <xkeshav@gmail.com>
2024-03-08 18:41:27 +0530

    ts package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const alphabet = Array.from(Array(26), (_: any, i:number) => String.fromCharCode(65 + i));
+export const alphabet = Array.from(Array(26), (_: undefined, i:number) => String.fromCharCode(65 + i));
