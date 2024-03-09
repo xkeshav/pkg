219233d023dd4d8d70825321d64ec2112bf28032
Keshav Mohta <xkeshav@gmail.com>
2024-03-09 19:49:32 +0530

    added few utility method

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,4 @@
 export declare const alphabet: string[];
+export declare const isNumber: (num: number) => boolean;
+export declare const isAlphabet: (num: number) => boolean;
+export declare const getRandomValueFrom: (a?: any) => any;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const alphabet = Array.from(Array(26), (_: undefined, i:number) => String.fromCharCode(65 + i));
+// return an array containing all english alphabets in capital case.
+const alphabet = Array.from(Array(26), (_: undefined, i:number) => String.fromCharCode(65 + i));
+// check in capital and small alphabet range
+const isAlphabet = (key: number) => (65 <= key && key <= 90) || (key >= 97 && key <= 122);
+// check whether given input is number or not ( i.e. number between 0 to 9)
+const isNumber = (key: number) => 48 <= key && key <= 57;
+// get random value from given array
+const getRandomValueFrom = (array: any = []) => array[Math.floor(Math.random() * array.length)];
+
+export { alphabet, getRandomValueFrom, isAlphabet, isNumber };
